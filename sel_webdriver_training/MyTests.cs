3196a377c34755a20a7931c2383aaf25811c181b
galinaborisenko <fleur.galya@gmail.com>
2021-05-19 13:49:22 +0200

    Task 1. FirstTest

---
+++ sel_webdriver_training/MyTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace sel_webdriver_training
+{
+    [TestFixture]
+    public class MyTests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "https://www.google.com/";
+            driver.FindElement(By.Id("L2AGLb")).Click();
+            driver.FindElement(By.Name("q")).SendKeys("webdriver" + Keys.Enter);
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
+
